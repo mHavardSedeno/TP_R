662deffdbf5bc7fb0a85d43292e9847959764a2c
mHavardSedeno <49910563+mHavardSedeno@users.noreply.github.com>
2020-10-09 11:23:56 +0200

    Corr FREQ_tbl avec MotSuivant

--- TP - Distantiel.R
+++ TP - Distantiel.R
@@ -1,7 +1,6 @@
 #TP - Distantiel - Date limite 9 octobre
 #1. 
-texte <- scan(file = "cyrano.txt", what = "character", n = 40)
-
+texte <- scan(file = "cyrano.txt", what = "character", n=1000)
 #a. Écrire le code permettant d’obtenir la liste des mots d’un fichier texte, dans leur ordre d’apparition.
 
 freq <- table(texte);freq
@@ -13,30 +12,31 @@ length(PlusFreq)
 DixPlusFreq = PlusFreq[1:10]; DixPlusFreq
 
 #c.1 Tracer un (ou plusieurs) graphique montrant que peu de mots accaparent l’essentiel du texte.
-plot(DixPlusFreq, main="Fréquence d'apparition des mots dans un extrait de Cyrano", xlab="Mots", ylab="Ocurence", col = rainbow(5))
-hist(DixPlusFreq, breaks=2, main="Fréquence d'apparition des mots dans un extrait de Cyrano", xlab="Mots", ylab="Ocurence", col = rainbow(5))
+#plot(DixPlusFreq, main="Fréquence d'apparition des mots dans un extrait de Cyrano", xlab="Mots", ylab="Ocurence", col = rainbow(5))
+#hist(DixPlusFreq, breaks=2, main="Fréquence d'apparition des mots dans un extrait de Cyrano", xlab="Mots", ylab="Ocurence", col = rainbow(5))
+
 #c.2 Écrire le code permettant d’obtenir la matrice FREQ
 
 motAvant <- texte
 motApres <- c(motAvant[2:length(texte)],"NA");motApres
 
 
-FREQ <- matrix(0, nrow = length(freq), ncol = length(freq));FREQ
+FREQ_tbl <- matrix(0, nrow = length(freq), ncol = length(freq));FREQ_tbl
 
-FreqMotSuivant <- function(motAv,motApr){
-  compt = 0
-  for(i in 1:(length(texte)-1)) {
-    if (texte[i] == motAv & texte[i+1] == motApr){
-      compt = compt + 1
-    }
-  }
-  return(compt)
-}
+#FreqMotSuivant <- function(motAv,motApr){
+#  compt = 0
+#  for(i in 1:(length(texte)-1)) {
+#    if (texte[i] == motAv & texte[i+1] == motApr){
+#      compt = compt + 1
+#    }
+#  }
+#  return(compt)
+#}
 
 FreqMotSuivant1 <- function(motAv,motApr){
   compt = 0
   for(i in 1:length(texte)) {
-    if (motApres[i] == motApr){
+    if (motAvant[i] == motAv & motApres[i] == motApr){
       compt = compt + 1
     }
   }
@@ -47,10 +47,11 @@ for (i in 1:length(freq)){
   for (j in 1:length(freq)){
     if (i != j){
       #FREQ[i,j] = FreqMotSuivant(names(freq[i]),names(freq[j]))
-      FREQ[i,j] = FreqMotSuivant1(names(freq[i]),names(freq[j]))
+      FREQ_tbl[i,j] = FreqMotSuivant1(names(freq[i]),names(freq[j]))
     }
   }
-};FREQ
+};
+FREQ_tbl
 
 #Puis écrire une fonction qui, à partir de FREQ, donne une fonction fournissant le mot le plus vraisemblable
 #après un mot donné (ou une chaîne vide si aucune proposition n’est possible).
@@ -63,16 +64,17 @@ prochainMot <- function(mot){
     }
   }
   
+
   MotProchain = 0
   
   for(j in 1:length(freq)) {
-    if (MotProchain < FREQ[ligne,j]){
+    if (MotProchain < FREQ_tbl[ligne,j]){
       MotProchain = j
     }
   }
   return(names(freq[MotProchain]))
-  #return(MotProchain)
 }
 
+start.time <- Sys.time()
 mp <- prochainMot("de");mp
-
+end.time <- Sys.time() - start.time
